b0c58c6d3a697f741326f45f2163e5eb14ff52f0
Manuel Filz <manuel.filz@gmx.at>
2015-07-09 11:26:34 +0200

    created module

---
+++ event-processing/src/main/java/at/prototype/event/processing/App.java
@@ -0,0 +1,13 @@
+package at.prototype.event.processing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ iot-device/src/main/java/at/prototype/iot/device/App.java
@@ -0,0 +1,13 @@
+package at.prototype.iot.device;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
